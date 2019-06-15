c8dc4d09b8dc8cad49c2beee001bef8913cd2736
Aayush Sharma <aayushsharma889@gmail.com>
2019-06-15 16:27:58 +0200

    Updates

---
+++ data/data.js
@@ -0,0 +1,73 @@
+export default [
+    {
+        id: 101,
+        name: "Stanca Pacepa",
+        image: "/src/data/stanca-pacepa",
+        region: "Romania",
+        price: 200
+    },
+    {
+        id: 201,
+        name: "Rosa Maria Meza",
+        image: "/src/data/stanca-pacepa.jpg",
+        region: "Mexico",
+        price: 400
+    },
+    {
+        id: 301,
+        name: "Vinay Lungeli",
+        image: "/src/data/vinay-lungeli.jpg",
+        region: "Nepal",
+        price: 210
+    },
+    {
+        id: 401,
+        name: "Florica Bejenaru",
+        image: "/src/data/florica-bejenaru.jpg",
+        region: "Romania",
+        price: 500
+    },
+    {
+        id: 501,
+        name: "Romana Bartovic",
+        image: "/src/data/romana-bartovic.jpg",
+        region: "Slovakia",
+        price: 100
+    },
+    {
+        id: 601,
+        name: "Adonic Ionescu",
+        image: "/src/data/adonic-ionescu.jpg",
+        region: "Romania",
+        price: 400
+    },
+    {
+        id: 701,
+        name: "Laura Thomsen",
+        image: "/src/data/laura-thompsen.jpg",
+        region: "Denmark",
+        price: 300
+    },
+
+    {
+        id: 801,
+        name: "Dylan Parker",
+        image: "/src/data/dylan-parker.jpg",
+        region: "New Zealand",
+        price: 700
+    },
+    {
+        id: 901,
+        name: "Anton Bauer",
+        image: "/src/data/anton-bauer.jpg",
+        region: "Germany",
+        price: 400
+    },
+    {
+        id: 1001,
+        name: "Monalisa Trandafir",
+        image: "/src/data/monalisa-trandafir.jpg",
+        region: "Romania",
+        price: 200
+    },
+    ]
---
+++ precache-manifest.f82a48b987a2181c0b433f4005c5382e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff9a4965565021fa9afcd72a45f69af5",
+    "url": "/robo-shop/index.html"
+  },
+  {
+    "revision": "444e208cdcb17595a0b5",
+    "url": "/robo-shop/static/css/main.54442a5b.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/robo-shop/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "444e208cdcb17595a0b5",
+    "url": "/robo-shop/static/js/main.36525749.chunk.js"
+  },
+  {
+    "revision": "3b18941fa08abaf00f02",
+    "url": "/robo-shop/static/js/runtime~main.77af1a55.js"
+  }
+]);
